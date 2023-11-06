c86f991e0b6924d315006d54abed76e000a4143e
Ansaar Ramkoleea <ansaar.r.h@gmail.com>
2023-11-06 23:46:50 +0400

    controllers

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,5 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  has_one :adoption
+  validates :breed, :name, :age, :ok_sterilised, :ok_vaccinated, :has_handicap, :ok_dog, :ok_cat, :ok_play, :ok_calm, :location, presence: true
 end
