383a3d0972ba83ab428840b9525f2fbac78783fd
Ansaar Ramkoleea <ansaar.r.h@gmail.com>
2023-11-08 16:59:24 +0400

    index page

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,8 +4,10 @@ class Animal < ApplicationRecord
 
   belongs_to :user
   has_one :adoption
-  validates :breed, :name, :age, :ok_vaccinated, :behaviour, :location, presence: true
-
+  # validates :breed, :name, :age, :ok_vaccinated, :behaviour, :location, presence: true
+  validates :breed, inclusion: { in: %w(dog cat)}
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  acts_as_taggable_on :tags
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 
   resources :animals
 
-
+  
 end
