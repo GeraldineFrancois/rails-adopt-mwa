1d382658b71bf5347da36d69a6bea84ff447cb7b
Ansaar Ramkoleea <ansaar.r.h@gmail.com>
2023-11-14 11:18:23 +0400

    filtering

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
 
 # destroying all before seeding again to avoid duplicates...
@@ -68,5 +61,7 @@ animal4.photos.attach(io: caramel2, filename: "IMG_3166_zy82qh", content_type: "
 animal4.photos.attach(io: caramel3, filename: "a4c4faef-4272-403e-9853-b3e3ceab7d36_oow47o", content_type: "image/jpg")
 
 adoption1 = Adoption.create(start_date: Date.today, status: "pending", user: user1, animal: animal1)
+adoption2 = Adoption.create(start_date: Date.today, status: "pending", user: user1, animal: animal3)
 
 Chatroom.create(name: "Pilou", adoption: adoption1)
+Chatroom.create(name: "Yoko", adoption: adoption2)
